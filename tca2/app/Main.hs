37797d27785acdc4fc3e1f0918309ae95a662045
Theoremprover <no@email.com>
2018-07-05 22:24:34 +0200

    No Message

---
+++ tca2/app/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
+-- http://localhost:8023
+-- https://www.stackage.org/haddock/lts-11.16/threepenny-gui-0.8.2.4/Graphics-UI-Threepenny.html
+
+main :: IO ()
+main = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup w = do
+	return w # set title "Test"
+	button <- UI.button # set UI.text "Button"
+	getBody w #+ [element button]
+	on UI.click button $ const $ do
+		element button # set UI.text "CLICKED!"
