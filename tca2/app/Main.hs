7d0fe7a1cac473765b9f31c7534f98eac621ea92
Reitmeier <reitmeier@validas.de>
2021-08-02 17:42:39 +0200

    ccyx

--- tca2/app/Main.hs
+++ tca2/app/Main.hs
@@ -1,16 +1,18 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}
+
 module Main where
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
 -- http://localhost:8023
--- https://www.stackage.org/haddock/lts-11.16/threepenny-gui-0.8.2.4/Graphics-UI-Threepenny.html
+
+data Tree a = Leaf a | Tree a [Tree a]
+
+example_tree = Tree "root" [ Leaf "left", Tree "right" [ Leaf "r1",Tree "r2" [ Leaf "leaf" ] ] ]
 
 main :: IO ()
-main = startGUI defaultConfig setup
-
-setup :: Window -> UI ()
-setup w = do
+main = startGUI defaultConfig $ \ w -> do
 	return w # set title "Test"
 	button <- UI.button # set UI.text "Button"
 	input <- UI.input #. "input"
